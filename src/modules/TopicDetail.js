8c9bb7a6fb4c53244db1293bc1c2cea7ba82edd1
frankyeyq <frankyeyq19892@163.com>
2018-01-10 22:07:58 +0800

    reply

--- src/modules/TopicDetail.js
+++ src/modules/TopicDetail.js
@@ -5,31 +5,39 @@ class TopicDetail extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: []
+            data: [],
+            replyLis: []
         }
-        this.getData()
     }
     getData() {
         let id = this.props.match.params.id
+
         topicDetail({id}).then(res => {
             if (res.data.success) {
                 this.setState({
                     data: res.data.data
                 })
+                let replyLis = this.state.data.replies.map((item) => 
+                    <li className="reply-li" key={item.id}>
+                        <img src={item.author.avatar_url} />
+                        <div className="reply-detail"></div>
+                    </li>
+                )
+                console.log(replyLis)
+                this.setState({
+                    replyLis
+                })
             }
             console.log(res)
         })
     }
+    componentDidMount() {
+        this.getData()
+    }
     createMarkup() {
-        console.log(this)
         return {__html: this.state.data.content}
     }
     render() {
-        let replyLis = this.state.data.replies.map(item => {
-            <li className="reply-li" key={item.id}>
-                <img src={item.author.avatar_url} />
-            </li>
-        })
         return (
             <div className="topic-detail">
                 <div className="header">
@@ -41,7 +49,7 @@ class TopicDetail extends React.Component {
                 <div className="replies">
                     <p className="reply-num"></p>
                     <ul className="reply-ul">
-                        {replyLis}
+                        {this.state.replyLis}
                     </ul>
                 </div>
             </div>
